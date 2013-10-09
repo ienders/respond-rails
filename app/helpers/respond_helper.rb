7cbd94a210bad8dd2b3785d7d476bb611d307f19
Ian Enders <ian.enders@gmail.com>
2013-10-09 15:25:44 -0400

    Better asset paths. No more protocol schemas.

--- app/helpers/respond_helper.rb
+++ app/helpers/respond_helper.rb
@@ -1,12 +1,23 @@
 module RespondHelper
 
   def respond_include_tags
-    tags = [javascript_include_tag(:respond)]
+    respond_js = javascript_path('respond.proxy.js')
+    respond_js.gsub! /^\/\//, request.protocol
+    tags = [ javascript_include_tag(respond_js) ]
 
     unless ActionController::Base.asset_host.nil?
-      tags << tag('link', {:href => asset_path('respond-proxy.html'), :id => 'respond-proxy', :rel => 'respond-proxy'})
-      tags << tag('link', {:href => image_path('respond.proxy.gif'), :id => 'respond-redirect', :rel => 'respond-redirect'})
-      tags << javascript_include_tag(javascript_path('respond.proxy.js'))
+      proxy_html = asset_path('respond-proxy.html')
+      proxy_html.gsub! /^\/\//, request.protocol
+
+      proxy_gif = image_path('respond.proxy.gif')
+      proxy_gif.gsub! ActionController::Base.asset_host, "#{request.protocol}#{request.host_with_port}"
+
+      proxy_js = javascript_path('respond.proxy.js')
+      proxy_js.gsub! ActionController::Base.asset_host, "#{request.protocol}#{request.host_with_port}"
+
+      tags << tag('link', :href => proxy_html, :id => 'respond-proxy', :rel => 'respond-proxy')
+      tags << tag('link', :href => proxy_gif, :id => 'respond-redirect', :rel => 'respond-redirect')
+      tags << javascript_include_tag(proxy_js)
     end
 
     tags.join("\n").html_safe
